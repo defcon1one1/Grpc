6ff188bcc97aea703ca0e2a4c74b60dad7b77c0a
Jan Bzdyl <94219390+defcon1one1@users.noreply.github.com>
2023-07-16 21:09:37 +0200

    Add files via upload

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System.Xml.Linq;
+using static GrpcService1.Greeter;
+
+using (var channel = GrpcChannel.ForAddress("http://localhost:5118"))
+{
+    var client = new GreeterClient(channel);
+    var request = new HelloRequest { Name = "Janek" };
+    var response = await client.SayHelloAsync(request);
+    Console.WriteLine(response.Message);
+}
